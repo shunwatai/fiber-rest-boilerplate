328b9c2460a4572ddd2af289de63230cfebaf30a
Sam Tai <shunwatai@gmail.com>
2023-11-07 22:38:18 +0800

    refactor(mariadb): select() use bindvar. handle date filter

--- internal/database/mariadb.go
+++ internal/database/mariadb.go
@@ -2,7 +2,10 @@ package database
 
 import (
 	"fmt"
+	"golang-api-starter/internal/helper"
 	"log"
+	"math"
+	"strconv"
 	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -12,6 +15,7 @@ import (
 type MariaDb struct {
 	*ConnectionInfo
 	TableName string
+	db        *sqlx.DB
 }
 
 func (m *MariaDb) Connect() *sqlx.DB {
@@ -28,53 +32,114 @@ func (m *MariaDb) Connect() *sqlx.DB {
 	return db
 }
 
-func (m *MariaDb) Select(queries map[string]interface{}) *sqlx.Rows {
-	fmt.Printf("select from MariaDB, table: %+v\n", m.TableName)
-	db := m.Connect()
-	defer db.Close()
+// return select statment and *pagination by the req querystring
+func (m *MariaDb) constructSelectStmtFromQuerystring(
+	queries map[string]interface{},
+) (string, *helper.Pagination, map[string]interface{}) {
+	exactMatchCols := map[string]bool{"id": true} // default id(PK) have to be exact match
+	if queries["exactMatch"] != nil {
+		for k := range queries["exactMatch"].(map[string]bool) {
+			exactMatchCols[k] = true
+		}
+	}
 
-	selectStmt := fmt.Sprintf(
-		"SELECT * FROM %s",
-		m.TableName,
-	)
+	bindvarMap := map[string]interface{}{}
+	cols := m.GetColumns()
+	pagination := helper.GetPagination(queries)
+	dateRangeStmt := getDateRangeStmt(queries, bindvarMap)
+	fmt.Printf("dateRangeStmt: %+v, len: %+v\n", dateRangeStmt, len(dateRangeStmt))
+	helper.SanitiseQuerystring(cols, queries)
+
+	countAllStmt := fmt.Sprintf("SELECT COUNT(*) FROM %s", m.TableName)
+	selectStmt := fmt.Sprintf(`SELECT * FROM %s`, m.TableName)
 
 	fmt.Printf("queries: %+v, len: %+v\n", queries, len(queries))
-	if len(queries) != 0 { // add where clause
+	if len(queries) != 0 || len(dateRangeStmt) != 0 { // add where clause
 		whereClauses := []string{}
 		for k, v := range queries {
 			fmt.Printf("%+v: %+v(%T)\n", k, v, v)
 			switch v.(type) {
 			case []string:
-				whereClauses = append(whereClauses, fmt.Sprintf("%s IN ('%s')", k, strings.Join(v.([]string), "','")))
+				placeholders := []string{}
+				if exactMatchCols[k] || strings.Contains(k, "_id") {
+					for i, value := range v.([]string) {
+						key := fmt.Sprintf(":%s%d", k, i+1)
+						bindvarMap[key[1:]] = value
+						placeholders = append(placeholders, key)
+					}
+					whereClauses = append(whereClauses, fmt.Sprintf("%s IN (%s)",
+						k, strings.ToLower(strings.Join(placeholders, ",")),
+					))
+					break
+				}
+
+				multiLikeClause := []string{}
+				for i, value := range v.([]string) {
+					key := fmt.Sprintf("%s%d", k, i+1)
+					bindvarMap[key] = fmt.Sprintf("%%%s%%", value)
+					multiLikeClause = append(multiLikeClause, fmt.Sprintf("lower(%s) LIKE :%s", k, key))
+				}
+				whereClauses = append(whereClauses,
+					fmt.Sprintf("(%s)", strings.ToLower(strings.Join(multiLikeClause, " OR "))),
+				)
 			default:
-				whereClauses = append(whereClauses, fmt.Sprintf("%s='%s'", k, v))
+				if exactMatchCols[k] || strings.Contains(k, "_id") {
+					bindvarMap[k] = v
+					whereClauses = append(whereClauses, fmt.Sprintf("%s=:%s", k, k))
+					break
+				}
+
+				bindvarMap[k] = fmt.Sprintf("%%%s%%", v)
+				whereClauses = append(whereClauses, strings.ToLower(fmt.Sprintf("%s LIKE :%s", k, k)))
 			}
 		}
 
-		selectStmt = fmt.Sprintf("%s WHERE %s", selectStmt, strings.Join(whereClauses, " AND "))
+		if len(dateRangeStmt) > 0 {
+			whereClauses = append(whereClauses, dateRangeStmt)
+		}
+		selectStmt = fmt.Sprintf("%s WHERE %s ", selectStmt, strings.Join(whereClauses, " AND "))
+		countAllStmt = fmt.Sprintf("%s WHERE %s", countAllStmt, strings.Join(whereClauses, " AND "))
 	}
 
-	fmt.Printf("selectStmt: %+v\n", selectStmt)
-	rows, err := db.Queryx(selectStmt)
-	if err != nil {
-		log.Printf("Queryx err: %+v\n", err.Error())
+	if totalRow, err := m.db.NamedQuery(countAllStmt, bindvarMap); err != nil {
+		log.Printf("Queryx Count(*) err: %+v\n", err.Error())
+	} else if totalRow.Next() {
+		defer totalRow.Close()
+		totalRow.Scan(&pagination.Count)
+	}
+	if pagination.Items > 0 {
+		pagination.TotalPages = int64(math.Ceil(float64(pagination.Count) / float64(pagination.Items)))
+	}
+	// fmt.Printf("pagination: %+v\n", pagination)
+
+	var limit string
+	var offset string = strconv.Itoa(int((pagination.Page - 1) * pagination.Items))
+	if pagination.Items == 0 {
+		limit = strconv.Itoa(int(pagination.Count))
+	} else {
+		limit = strconv.Itoa(int(pagination.Items))
 	}
 
-	if rows.Err() != nil {
-		log.Printf("rows.Err(): %+v\n", err.Error())
-	}
+	selectStmt = fmt.Sprintf(`%s 
+			ORDER BY %s %s
+			LIMIT %s OFFSET %s
+		`,
+		selectStmt,
+		pagination.OrderBy["key"], pagination.OrderBy["by"],
+		limit, offset,
+	)
 
-	return rows
+	return selectStmt, pagination, bindvarMap
 }
 
-func (m *MariaDb) Save(records Records) *sqlx.Rows {
-	fmt.Printf("save from MariaDB, table: %+v\n", m.TableName)
-	// fmt.Printf("records: %+v\n", records)
-	db := m.Connect()
-	defer db.Close()
+func (m *MariaDb) GetColumns() []string {
 	selectStmt := fmt.Sprintf("select * from %s limit 1;", m.TableName)
 
-	rows, err := db.Queryx(selectStmt)
+	if m.db == nil { // for run the test case
+		m.db = m.Connect()
+	}
+
+	rows, err := m.db.Queryx(selectStmt)
 	defer rows.Close()
 	if err != nil {
 		log.Printf("%+v\n", err)
@@ -85,11 +150,46 @@ func (m *MariaDb) Save(records Records) *sqlx.Rows {
 		log.Printf("%+v\n", err)
 	}
 
+	return cols
+}
+
+func (m *MariaDb) Select(queries map[string]interface{}) (*sqlx.Rows, *helper.Pagination) {
+	fmt.Printf("select from MariaDB, table: %+v\n", m.TableName)
+	m.db = m.Connect()
+	defer m.db.Close()
+
+	selectStmt, pagination, bindvarMap := m.constructSelectStmtFromQuerystring(queries)
+	fmt.Printf("bindvarMap: %+v\n", bindvarMap)
+	fmt.Printf("selectStmt: %+v\n", selectStmt)
+
+	rows, err := m.db.NamedQuery(selectStmt, bindvarMap)
+	if err != nil {
+		log.Printf("Queryx err: %+v\n", err.Error())
+	}
+
+	if rows.Err() != nil {
+		log.Printf("rows.Err(): %+v\n", err.Error())
+	}
+
+	return rows, pagination
+}
+
+func (m *MariaDb) Save(records Records) *sqlx.Rows {
+	fmt.Printf("save from MariaDB, table: %+v\n", m.TableName)
+	m.db = m.Connect()
+	defer m.db.Close()
+
+	cols := m.GetColumns()
+
 	// fmt.Printf("cols: %+v\n", cols)
 	var colWithColon, colUpdateSet []string
 	for _, col := range cols {
 		// use in SQL's VALUES()
-		colWithColon = append(colWithColon, fmt.Sprintf(":%s", col))
+		if strings.Contains(col, "_at") {
+			colWithColon = append(colWithColon, fmt.Sprintf("IFNULL(:%s, CURRENT_TIMESTAMP)", col))
+		} else {
+			colWithColon = append(colWithColon, fmt.Sprintf(":%s", col))
+		}
 
 		// use in SQL's ON DUPLICATE KEY UPDATE
 		if strings.Contains(col, "_at") {
@@ -112,7 +212,7 @@ func (m *MariaDb) Save(records Records) *sqlx.Rows {
 	fmt.Printf("%+v \n", insertStmt)
 
 	insertedIds := []string{}
-	sqlResult, err := db.NamedQuery(insertStmt, records)
+	sqlResult, err := m.db.NamedQuery(insertStmt, records)
 	if err != nil {
 		log.Printf("insert error: %+v\n", err)
 	}
@@ -128,7 +228,8 @@ func (m *MariaDb) Save(records Records) *sqlx.Rows {
 	}
 
 	fmt.Printf("insertedIds: %+v\n", insertedIds)
-	return m.Select(map[string]interface{}{"id": insertedIds})
+	rows, _ := m.Select(map[string]interface{}{"id": insertedIds})
+	return rows
 }
 
 // func (m *MariaDb) Update() {
@@ -136,8 +237,8 @@ func (m *MariaDb) Save(records Records) *sqlx.Rows {
 // }
 func (m *MariaDb) Delete(ids *[]int64) error {
 	fmt.Printf("delete from MariaDB, table: %+v\n", m.TableName)
-	db := m.Connect()
-	defer db.Close()
+	m.db = m.Connect()
+	defer m.db.Close()
 
 	deleteStmt, args, err := sqlx.In(
 		fmt.Sprintf("DELETE FROM %s WHERE id IN (?);", m.TableName),
@@ -147,14 +248,30 @@ func (m *MariaDb) Delete(ids *[]int64) error {
 		log.Printf("sqlx.In err: %+v\n", err.Error())
 		return err
 	}
-	deleteStmt = db.Rebind(deleteStmt)
+	deleteStmt = m.db.Rebind(deleteStmt)
 	fmt.Printf("stmt: %+v, args: %+v\n", deleteStmt, args)
 
-	_, err = db.Exec(deleteStmt, args...)
+	_, err = m.db.Exec(deleteStmt, args...)
 	if err != nil {
 		log.Printf("Delete Query err: %+v\n", err.Error())
 		return err
 	}
 
 	return nil
+}
+
+func (m *MariaDb) RawQuery(sql string) *sqlx.Rows {
+	fmt.Printf("raw query from Postgres\n")
+	m.db = m.Connect()
+	defer m.db.Close()
+
+	rows, err := m.db.Queryx(sql)
+	if err != nil {
+		log.Printf("Queryx err: %+v\n", err.Error())
+	}
+	if rows.Err() != nil {
+		log.Printf("rows.Err(): %+v\n", err.Error())
+	}
+
+	return rows
 }
