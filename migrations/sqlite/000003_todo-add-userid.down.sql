9073ede21965ccae84fbd4629f123f827628a5cb
Sam Tai <shunwatai@gmail.com>
2023-12-03 14:57:57 +0800

    migrate(sqlite): add users & add userId into todos

---
+++ migrations/sqlite/000003_todo-add-userid.down.sql
@@ -0,0 +1,34 @@
+CREATE TEMPORARY TABLE temp AS
+SELECT 
+    id,
+    task,
+    done,
+    created_at,
+    updated_at
+FROM todos;
+
+DROP TABLE todos;
+
+CREATE TABLE "todos" (
+	"id"	INTEGER NOT NULL,
+	"task"	TEXT,
+	"done"	NUMERIC,
+	"created_at"	DATETIME,
+	"updated_at"	DATETIME,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+INSERT INTO todos
+ (  id,
+    task,
+    done,
+    created_at,
+    updated_at
+  )
+SELECT
+    id,
+    task,
+    done,
+    created_at,
+    updated_at
+FROM temp;
