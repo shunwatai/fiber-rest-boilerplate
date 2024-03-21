d0c8523174c4478cb5e339fd9f628b84d2fc9abe
Sam Tai <shunwatai@gmail.com>
2024-03-21 15:05:41 +0800

    chore(users): update users's migration for the additional email columns (for reset-pw flow)

--- migrations/mongodb/000002_users.up.sql
+++ migrations/mongodb/000002_users.up.sql
@@ -3,6 +3,21 @@
     "create": "users"
   },
   {
+    "createIndexes": "users",
+    "indexes": [
+      {
+        "key": { "name": 1 },
+        "name": "name_index",
+        "unique": true
+      },
+      {
+        "key": { "email": 1 },
+        "name": "email_index",
+        "unique": true
+      }
+    ]
+  },
+  {
     "insert": "users",
     "documents": [
       {
@@ -10,7 +25,8 @@
         "first_name": null,
         "last_name": null,
         "name": "admin",
-        "password": "$2a$04$vRnhDEbQZx9OD0gRzGqWOO0Kii2XqWAyzPRCW2fOKqLhnNVMn2I/u"
+        "password": "$2a$04$vRnhDEbQZx9OD0gRzGqWOO0Kii2XqWAyzPRCW2fOKqLhnNVMn2I/u",
+        "email": "admin@example.com"
       }
     ]
   }
