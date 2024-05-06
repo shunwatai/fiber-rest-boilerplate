d9727fe148a3010245803357e318cb294110dab4
Sam Tai <shunwatai@gmail.com>
2024-05-06 12:15:13 +0800

    feat(group): map GroupUser's User as []User for Group response

--- internal/modules/group/repository.go
+++ internal/modules/group/repository.go
@@ -4,6 +4,10 @@ import (
 	"golang-api-starter/internal/database"
 	"golang-api-starter/internal/helper"
 	logger "golang-api-starter/internal/helper/logger/zap_log"
+	"golang-api-starter/internal/helper/utils"
+	"golang-api-starter/internal/modules/groupUser"
+	"golang-api-starter/internal/modules/user"
+
 	//"golang-api-starter/internal/modules/user"
 	"golang.org/x/exp/maps"
 )
@@ -18,17 +22,47 @@ func NewRepository(db database.IDatabase) *Repository {
 
 // cascadeFields for joining other module, see the example in internal/modules/todo/repository.go
 func cascadeFields(groups Groups) {
-  if len(groups) == 0 {
-    return
-  }
-	// cascade user
+	if len(groups) == 0 {
+		return
+	}
+
+	// cascade group-users
+	var (
+		groupIds []string
+		groupId  string
+	)
+	// get all todoId
+	for _, group := range groups {
+		groupId = group.GetId()
+		groupIds = append(groupIds, groupId)
+	}
+
+	// get groups by groupIds
+	condition := database.GetIdsMapCondition(utils.ToPtr("group_id"), groupIds)
+	groupUsers, _ := groupUser.Srvc.Get(condition)
+	groupUsersMap := groupUser.Srvc.GetGroupIdMap(groupUsers)
+
+	for _, group := range groups {
+		// if no users assign empty slice for response json "users": [] instead of "users": null
+		group.Users = []*user.User{}
+		// take out the groupUsers by groupId in map and assign
+		gus, haveUsers := groupUsersMap[group.GetId()]
+
+		if !haveUsers {
+			continue
+		} else {
+			for _, gu := range gus {
+				group.Users = append(group.Users, gu.User)
+			}
+		}
+	}
 }
 
 func (r *Repository) Get(queries map[string]interface{}) ([]*Group, *helper.Pagination) {
 	logger.Debugf("group repo get")
 	defaultExactMatch := map[string]bool{
-		"id":   true,
-		"_id":  true,
+		"id":  true,
+		"_id": true,
 		//"done": true, // bool match needs exact match, param can be 0(false) & 1(true)
 	}
 	if queries["exactMatch"] != nil {
@@ -46,7 +80,7 @@ func (r *Repository) Get(queries map[string]interface{}) ([]*Group, *helper.Pagi
 	}
 	// records.printValue()
 
-	//cascadeFields(records)
+	cascadeFields(records)
 
 	return records, pagination
 }
@@ -77,6 +111,7 @@ func (r *Repository) Update(groups []*Group) ([]*Group, error) {
 		records = records.rowsToStruct(rows)
 	}
 	records.printValue()
+	// cascadeFields(records)
 
 	return records, err
 }
