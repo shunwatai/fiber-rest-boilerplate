c9ee0c4b31f4521739d5c9a4cf7c2a5138b5a545
Sam Tai <shunwatai@gmail.com>
2024-05-22 14:02:14 +0800

    feat(rbac): add permissionCheck middleware

--- internal/helper/http.go
+++ internal/helper/http.go
@@ -1,5 +1,10 @@
 package helper
 
+import (
+	"github.com/gofiber/fiber/v2"
+	"strings"
+)
+
 type HttpErr struct {
 	Code int
 	Err  error
@@ -13,4 +18,20 @@ func (ctx *FiberCtx) JsonResponse(respCode int, data map[string]interface{}) err
 	return ctx.Fctx.
 		Status(respCode).
 		JSON(data)
+}
+
+func (ctx *FiberCtx) ErrResponse(respCode int, err error) error {
+	reqHeader := ctx.Fctx.GetReqHeaders()
+	isHtml := strings.Contains(reqHeader["Accept"][0], "text/html")
+
+	if isHtml {
+		ctx.Fctx.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
+		return ctx.Fctx.
+			Status(respCode).
+			Redirect("/error", fiber.StatusPermanentRedirect)
+	}
+
+	return ctx.Fctx.
+		Status(respCode).
+		JSON(map[string]interface{}{"message": err.Error()})
 }
