997fefc68bc0e6198fa5ca995c23985fa8b0ec3d
Sam Tai <shunwatai@gmail.com>
2024-11-01 02:42:44 +0800

    refactor(resource+permission-type): change Order to Seq to prevent conflict with sql keyword

--- migrations/sqlite/000013_create_permission_types.up.sql
+++ migrations/sqlite/000013_create_permission_types.up.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS "permission_types" (
 	"id"	INTEGER NOT NULL,
 	"name"	TEXT NOT NULL,
-	"order"	INTEGER NOT NULL,
+	"seq"	INTEGER NOT NULL,
 	"created_at"	DATETIME NOT NULL,
 	"updated_at"	DATETIME NOT NULL,
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
 -- Pre-populate some default permissions
-INSERT INTO "permission_types" ("id", "name", "order", "created_at", "updated_at") VALUES
+INSERT INTO "permission_types" ("id", "name", "seq", "created_at", "updated_at") VALUES
 (1,	'read',	 1,'2024-05-15 05:41:33.433213+00',	'2024-05-15 05:41:33.433213+00'),
 (2,	'add',	 2,'2024-05-15 05:41:36.086894+00',	'2024-05-15 05:41:36.086894+00'),
 (3,	'edit',	 3,'2024-05-15 05:41:42.740808+00',	'2024-05-15 05:41:42.740808+00'),
